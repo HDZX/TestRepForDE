ed73bf29906951b81fffea2c5d18eb62a23eee04
Constanta <dimon619@list.ru>
2020-03-26 23:26:20 +0200

    Update

---
+++ SpecflowTestProject/NewBookModelsPOM/ClientSignUp.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+
+namespace SpecflowTestProject.NewBookModelsPOM
+{
+    public class ClientSignUp
+    {
+        private readonly IWebDriver _driver;
+
+        public ClientSignUp(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        private static readonly By FinishBtn = By.CssSelector("button[class^=SignupCompany]");
+
+        public bool IsFinishBtnDisplayed()
+        {
+            return _driver.FindElement(FinishBtn).Displayed;
+        }
+    }
+}
---
+++ SpecflowTestProject/Steps/LoginSteps.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using SpecflowTestProject.NewBookModelsPOM;
+using TechTalk.SpecFlow;
+using TestApiProject.ApiRequests.Models;
+using TestApiProject.ApiRequests.NewBookModelsApi.Auth;
+
+namespace SpecflowTestProject
+{
+    [Binding]
+    public class LoginSteps
+    {
+        private readonly IWebDriver _driver = (IWebDriver)ScenarioContext.Current["driver"];
+
+        [Given(@"Client is created")]
+        public void GivenClientIsCreated()
+        {
+            ScenarioContext.Current["client"] = new AuthRequests().SendRequestSignUpPost(new ClientSignUpModel
+            {
+                Email = "qweasd" + DateTime.Now.ToString("hhmmss") + "asd@gmail.com",
+                Password = "123qweQWE!",
+                FirstName = "sdfsadfsf",
+                LastName = "asdadasdsad",
+                PhoneNumber = "1231231231"
+            });
+        }
+
+        [Given(@"Sign in page is opened")]
+        public void GivenSignInPageIsOpened()
+        {
+            new SignInPage(_driver).SignInPageIsOpened();
+        }
+
+        [When(@"I input email of created client in email field")]
+        public void WhenIInputEmailOfCreatedClientInEmailField()
+        {
+            var clientEmail = ((ClientAuthModel)ScenarioContext.Current["client"]).User.Email;
+            new SignInPage(_driver).FillEmailField(clientEmail);
+        }
+
+        [When(@"I input password of created client in email field")]
+        public void WhenIInputPasswordOfCreatedClientInEmailField()
+        {
+            new SignInPage(_driver).FillPasswordField("123qweQWE!");
+        }
+
+        [When(@"I click Log in button")]
+        public void WhenIClickLogInButton()
+        {
+            new SignInPage(_driver).ClickLogInBtn();
+        }
+
+        [Then(@"I successfully logged in NewBookModels as created client")]
+        public void ThenISuccessfullyLoggedInNewBookModelsAsCreatedClient()
+        {
+            Thread.Sleep(5000);
+            Assert.True(new ClientSignUp(_driver).IsFinishBtnDisplayed());
+        }
+    }
+}
